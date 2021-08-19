5cde0bd271512f197a2b999e287c1dea07bc4d5b
China-Htf <837183807@qq.com>
2021-08-19 09:03:41 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
+// createStore({}) 创建仓库
 export default createStore({
   state: {
+    // 存放数据
   },
   mutations: {
+    // 改数据函数
   },
   actions: {
+    // 请求数据函数
   },
   modules: {
-  }
-})
+    // 分模块
+  },
+  getters: {
+    // vuex 的计算属性
+  },
+});
